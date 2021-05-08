f3ffab316530bf547cba9b87f777e5ab51f3b8ed
paparayo <guswind8@gmail.com>
2021-05-08 23:35:29 +0900

    ADD autoCreationPopup. js

--- src/js/view/popup/autoScheduleCreationPopup.js
+++ src/js/view/popup/autoScheduleCreationPopup.js
@@ -1,9 +1,9 @@
 var personalWork = document.getElementById('btn-auto-schedule-creation');
 
 function popup() {
-    var url = "/../template/popup/autoScheduleCreationPopup.html";
+    var url = "autoScheduleCreationPopup.html";
     var name = "popup test";
-    var option = "width=500, height=500, location=no";
+    var option = "width=475, height=275, location=no";
     window.open(url, name, option);
 }
 
