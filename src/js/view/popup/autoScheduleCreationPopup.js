33b2bd0e68c18ba960583427cf1718ad91954dce
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-05-08 22:51:41 +0900

    ADD auto-schedule-popup

--- src/js/view/popup/autoScheduleCreationPopup.js
+++ src/js/view/popup/autoScheduleCreationPopup.js
@@ -1,12 +1,10 @@
 var personalWork = document.getElementById('btn-auto-schedule-creation');
 
 function popup() {
-    var url = "autoScheduleCreation";
+    var url = "/../template/popup/autoScheduleCreationPopup.html";
     var name = "popup test";
     var option = "width=500, height=500, location=no";
     window.open(url, name, option);
-
-    //$('#btn1').on('click', createNewSchedule);
 }
 
 personalWork.addEventListener('click', function() {
