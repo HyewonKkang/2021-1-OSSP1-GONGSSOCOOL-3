372d38d4e06125172725432b29c05218d98bf2ad
박소현 <shpark5938@naver.com>
2021-05-18 23:35:29 +0900

    ADD teamCreationPopup

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,29 +1,7 @@
 const schedule = require('./schedule');
-const user = require('./user');
 
 module.exports = {
     init(app) {
-
-        const whitelist = ['/login', '/register', '/loged'];
-
-        app.all('*', (req, res, next) => {
-            if (req.session.user) {
-                next();
-            } else {
-                let find = whitelist.find(function(regexp) {
-                    if (req.originalUrl.match(regexp)) {
-                        return true;
-                    }
-                });
-                if (find) {
-                    next();
-                } else {
-                    res.status(401).json({msg: 'Need login'});
-                }
-            }
-        });
-
         schedule.init(app);
-        user.init(app);
-    }
+    },
 };
