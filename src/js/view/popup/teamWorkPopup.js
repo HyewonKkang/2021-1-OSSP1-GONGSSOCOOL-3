768d6921ae17ffc454dd7a81fc81fe5e99dace79
박소현 <shpark5938@naver.com>
2021-05-21 00:17:52 +0900

    ADD teamCreationPopup, FIX teamWorkPopup

--- src/js/view/popup/teamWorkPopup.js
+++ src/js/view/popup/teamWorkPopup.js
@@ -3,7 +3,7 @@ var personalWork = document.getElementById('btn-team-work');
 function teamPopup() {
     var url = "teamWorkPopup.html";
     var name = "team-work";
-    var option = "width=475, height=195, location=no";
+    var option = "width=395, height=395, location=no";
 
     window.open(url, name, option);
 }
