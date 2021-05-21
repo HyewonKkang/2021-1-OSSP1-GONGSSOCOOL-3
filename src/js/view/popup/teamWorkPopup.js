d51527e103fd04e7e4f75c97bc2e9983fbb6d199
paparayo <guswind8@gmail.com>
2021-05-21 20:07:53 +0900

    UPDATE teamWorkPopup.html

--- src/js/view/popup/teamWorkPopup.js
+++ src/js/view/popup/teamWorkPopup.js
@@ -1,5 +1,29 @@
 var personalWork = document.getElementById('btn-team-work');
+/*
+var DatePicker = require('tui-date-picker');
+var timezone = require('../../common/timezone');
+var datetime = require('../../common/datetime');
+var TZDate = timezone.Date;
 
+
+teamWorkPopup.prototype._createDatepicker = function(start, end) {
+
+    this.rangePicker = DatePicker.createRangePicker({
+        startpicker: {
+            date: new TZDate(start).toDate(),
+            input: '#' + 'tui-full-calendar-schedule-start-date',
+            container: '#' + 'startpicker-container'
+        },
+        endpicker: {
+            date: new TZDate(end).toDate(),
+            input: '#' + 'tui-full-calendar-schedule-end-date',
+            container: '#' + 'endpicker-container'
+        },
+        
+        //usageStatistics: this._usageStatistics
+    });
+};
+*/
 function teamPopup() {
     var url = "teamWorkPopup.html";
     var name = "team-work";
@@ -8,6 +32,8 @@ function teamPopup() {
     window.open(url, name, option);
 }
 
+
 personalWork.addEventListener('click', function() {
+    //_createDatepicker();
     teamPopup();
 })
